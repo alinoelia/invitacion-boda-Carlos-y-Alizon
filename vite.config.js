6d7f900927592d0f5c4347d77c119c2bac0c8a78
aliz <aliznoelia@gmail.com>
2026-09-04 02:54:59 -0300

    Correcion de datos

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Invitacion-charlizon/',
+  base: '/invitacion-boda-Carlos-y-Alizon/',
   plugins: [react()],
 })
